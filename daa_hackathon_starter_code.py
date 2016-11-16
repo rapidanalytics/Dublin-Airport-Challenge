3c18b2915fcb3f15cb53967405d07ddbc6cd9595
shaneteehan <shane.teehan@daa.ie>
2016-11-16 09:30:06 +0000

    Add files via upload

--- daa_hackathon_starter_code.py
+++ daa_hackathon_starter_code.py
@@ -9,8 +9,9 @@ from sklearn.metrics import mean_squared_error
 ########################################################
 # 2. Define variables which we will use later in the script
 ########################################################
-s_roor_folder = 'S:/Public/ADA Team/Hackathon/'
-s_train_file = 'train.csv' 
+s_root_folder = 'S:/Public/ADA Team/Hackathon/'
+s_train_file = 'train.csv'
+s_test_file = 'test.csv'
 l_parse_date_cols = ['dt_prediction_date', 'dt_target_date', 'dt_flight_date']
 l_target_cols = ['num_pax_000_014_mins_before_sdt', 'num_pax_015_029_mins_before_sdt', 'num_pax_030_044_mins_before_sdt', 'num_pax_045_059_mins_before_sdt', 'num_pax_060_074_mins_before_sdt', 'num_pax_075_089_mins_before_sdt', 'num_pax_090_104_mins_before_sdt', 'num_pax_105_119_mins_before_sdt',  'num_pax_120_134_mins_before_sdt', 'num_pax_135_149_mins_before_sdt', 'num_pax_150_164_mins_before_sdt', 'num_pax_165_179_mins_before_sdt',  'num_pax_180_194_mins_before_sdt', 'num_pax_195_209_mins_before_sdt', 'num_pax_210_224_mins_before_sdt', 'num_pax_225_239_mins_before_sdt', 'num_pax_240plus_mins_before_sdt']
 
@@ -32,7 +33,7 @@ def calculate_score(df_target_cases, df_predictions):
 ###############################################
 # 4. Read in csv file and parse dates. Also generate dataframe with the target cases ordered by id
 ###############################################
-df_raw_data_amt = pd.read_csv(s_roor_folder + s_train_file, parse_dates = l_parse_date_cols)
+df_raw_data_amt = pd.read_csv(s_root_folder + s_train_file, parse_dates = l_parse_date_cols)
 df_target_cases = df_raw_data_amt[df_raw_data_amt['cat_case_type'] == 'Target'].set_index('id').sort_index()[l_target_cols]
 
 
@@ -74,3 +75,23 @@ df_combined_predictions = pd.concat([df_avg_prediction, df_zero_preds], ignore_i
 ###############################################
 f_rmse = calculate_score(df_target_cases, df_combined_predictions)
 print('The root-mean-squared error is ' + str(f_rmse))
+
+
+###############################################
+# 7. Apply the same approach to the test data and save the predictions to file for submission
+###############################################
+df_raw_data_amt = pd.read_csv(s_root_folder + s_test_file, parse_dates = l_parse_date_cols)
+df_target_cases = df_raw_data_amt[df_raw_data_amt['cat_case_type'] == 'Target'].set_index('id').sort_index()[l_target_cols]
+
+df_raw_data_amt['id_rank'] = df_raw_data_amt[df_raw_data_amt['cat_case_type'] == 'Expl'].groupby('id')['dt_flight_date'].rank(ascending = False)
+df_expl_data = df_raw_data_amt[df_raw_data_amt['id_rank'] <= 5][['id'] + l_target_cols]
+df_avg_prediction = df_expl_data.groupby('id').mean()
+df_avg_prediction = check_for_negatives_in_pred(df_avg_prediction, l_target_cols)
+
+b_mask = np.in1d(df_raw_data_amt[df_raw_data_amt['cat_case_type'] == 'Target']['id'].unique(), df_avg_prediction.index, assume_unique = True)
+arr_no_history_id = df_raw_data_amt[df_raw_data_amt['cat_case_type'] == 'Target']['id'].unique()[~b_mask]
+df_zero_preds = pd.DataFrame(index = arr_no_history_id, columns = l_target_cols)
+df_zero_preds = df_zero_preds.fillna(0)
+
+df_combined_predictions = pd.concat([df_avg_prediction, df_zero_preds], ignore_index = False).sort_index()
+df_combined_predictions.to_csv(s_root_folder + "model_submission.csv")
--- daa_hackathon_starter_code.r
+++ daa_hackathon_starter_code.r
@@ -8,7 +8,8 @@ library(dplyr)
 # 2. Define variables which we will use later in the script
 ########################################################
 s_root_folder <- 'S:/Public/ADA Team/Hackathon/'
-s_train_file <- 'train.csv' 
+s_train_file <- 'train.csv'
+s_test_file <- 'test.csv'
 v_target_cols <- c('num_pax_000_014_mins_before_sdt', 'num_pax_015_029_mins_before_sdt', 'num_pax_030_044_mins_before_sdt', 'num_pax_045_059_mins_before_sdt', 'num_pax_060_074_mins_before_sdt', 'num_pax_075_089_mins_before_sdt', 'num_pax_090_104_mins_before_sdt', 'num_pax_105_119_mins_before_sdt',  'num_pax_120_134_mins_before_sdt', 'num_pax_135_149_mins_before_sdt', 'num_pax_150_164_mins_before_sdt', 'num_pax_165_179_mins_before_sdt',  'num_pax_180_194_mins_before_sdt', 'num_pax_195_209_mins_before_sdt', 'num_pax_210_224_mins_before_sdt', 'num_pax_225_239_mins_before_sdt', 'num_pax_240plus_mins_before_sdt')
 
 
@@ -40,6 +41,7 @@ df_raw_data_amt$dt_flight_date <- as.Date(df_raw_data_amt$dt_flight_date, format
 
 df_target_cases <- df_raw_data_amt[df_raw_data_amt['cat_case_type'] == 'Target', c("id", v_target_cols)]
 df_target_cases <- df_target_cases[order(df_target_cases$id), ]
+
 
 ############################################################################################################################
 # 5. For demonstration purposes, we will make a prediction for each ID based only on the average passenger profile from the five most recent historical flight cases prior to the prediction date
@@ -81,3 +83,31 @@ df_combined_predictions <- df_combined_predictions[order(df_combined_predictions
 ###############################################
 f_rmse <- calculate_score(df_target_cases, df_combined_predictions, v_target_cols)
 print(paste('The root-mean-squared error is ', f_rmse, sep = ""))
+
+
+###############################################
+# 7. Apply the same approach to the test data and save the predictions to file for submission
+###############################################
+df_test_data_amt <- read.csv(s_test_file, header = TRUE, sep = ",")
+
+df_test_data_amt$dt_target_date <- as.Date(df_test_data_amt$dt_target_date, format = "%Y-%m-%d")
+df_test_data_amt$dt_prediction_date <- as.Date(df_test_data_amt$dt_prediction_date, format = "%Y-%m-%d")
+df_test_data_amt$dt_flight_date <- as.Date(df_test_data_amt$dt_flight_date, format = "%Y-%m-%d")
+
+df_target_cases <- df_test_data_amt[df_test_data_amt['cat_case_type'] == 'Target', c("id", v_target_cols)]
+df_target_cases <- df_target_cases[order(df_target_cases$id), ]
+
+df_expl_data <- df_test_data_amt[df_test_data_amt['cat_case_type'] == 'Expl', ]
+df_expl_data <- df_expl_data %>% group_by(id) %>% mutate(id_rank = order(dt_flight_date, decreasing=TRUE))
+df_expl_data <- df_expl_data[df_expl_data['id_rank'] <= 5, c('id', v_target_cols)]
+df_avg_prediction <- df_expl_data %>% group_by(id) %>% summarise_each(funs(mean))
+df_avg_prediction <- check_for_negatives_in_pred(df_avg_prediction, v_target_cols)
+
+v_no_history_id <- subset(df_target_cases$id, !(df_target_cases$id %in% df_avg_prediction$id))
+df_zero_preds <- data.frame(cbind(v_no_history_id, matrix(0, ncol = 17, nrow = length(v_no_history_id))))
+colnames(df_zero_preds) <- colnames(df_avg_prediction)
+
+df_combined_predictions <- rbind(df_avg_prediction, df_zero_preds)
+df_combined_predictions <- df_combined_predictions[order(df_combined_predictions$id), ]
+
+write.csv(x = df_combined_predictions, file = paste(s_root_folder, "model_submission.csv", sep = ""), row.names = FALSE)
